410f48f52d2e952eec1128c0cabeee35861883d9
xkwg <xkwg@163.com>
2020-11-26 10:54:40 +0800

    mysql配置

--- config/database.js
+++ config/database.js
@@ -21,7 +21,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),// strapi部署的服务器与MySQL所在的服务器是同一个服务器，主机就直接写localhost
+        host: env('DATABASE_HOST', 'liucong.icu'),// strapi部署的服务器与MySQL所在的服务器是同一个服务器，主机就直接写localhost
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'blog'),
